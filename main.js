a33e1443bdb321b422522b773a646489376f1472
jigyashagit <109466868+jigyashagit@users.noreply.github.com>
2022-12-11 17:20:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+lefteyeX=0;
+lefteyeY=0;
+function preload(){
+    redlips=loadImage("redlips.png");
+    sunglasses=loadImage("sunglasses.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = "+results[0].pose.nose.x);
+        console.log("nose y = "+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        
+    }
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+    image(redlips,noseX-25,noseY+5,50,50);
+    
+}
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
